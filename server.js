400c9ae1e775e053a6c335de3ab5d7d1808e7452
salonidarji <salonidarji@gmail.com>
2019-03-01 23:22:44 +0530

    updation for heroku

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var express = require('express'),
-app = express();
-app.use(express.static('./src/app'));
-app.get('/*',(req,res)=> {
-    res.sendFile(path.join(__dirname,'/src/index.html'));
+var express  = require('express');
+var app      = express();                               
+var morgan = require('morgan');            
+var bodyParser = require('body-parser');    
+var cors = require('cors');
+ 
+app.use(morgan('dev'));                                        
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
+app.use(cors());
+ 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
-
-app.listen(process.env.PORT || 8080, ()=>{
-    console.log('server started');
-})
+ 
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
